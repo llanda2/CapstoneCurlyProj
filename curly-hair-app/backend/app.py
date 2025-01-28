999000593d6914bec90ab870d2ac71389b8b1da4
Lauren Landa <laurenlanda@Lauren-Landa-MacBook-Pro.local>
2025-01-28 09:14:32 -0800

    uh

--- curly-hair-app/backend/app.py
+++ curly-hair-app/backend/app.py
@@ -1,10 +1,14 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 import pandas as pd
 
 app = Flask(__name__)
+CORS(app)
 
 # Load the CSV file
-df = pd.read_csv("/Users/laurenlanda/PycharmProjects/CapstoneCurlyProj/curly-hair-app/backend/data/Curly Hair Products - First Iteration.csv")
+df = pd.read_csv(
+    "/Users/laurenlanda/PycharmProjects/CapstoneCurlyProj/curly-hair-app/backend/data/Curly Hair Products - First Iteration.csv")
+
 
 @app.route("/quiz", methods=["POST"])
 def quiz():
@@ -21,11 +25,12 @@ def quiz():
         (df["Hair Type"].str.contains(hair_type, na=False)) &
         (df["Weight (L,M,H)"].str.contains(thickness, na=False)) &
         (df["Price"] <= price_map[price_range])
-    ]
+        ]
 
     # Select up to 3 products for the routine
     recommendations = filtered.head(3).to_dict(orient="records")
     return jsonify(recommendations)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
